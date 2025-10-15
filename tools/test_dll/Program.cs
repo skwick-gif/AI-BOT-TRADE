428adc801953b33f8f2fa39bf28321b187855e52
Eran Levy <skwick@gmail.com>
2025-10-15 23:03:44 +0300

    Remove leftover tracked 'TWS API' entries after local deletion

---
+++ tools/test_dll/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Testing IBKR DLL...");
+
+try
+{
+    var client = new IBApi.EClientSocket(null, null);
+    Console.WriteLine("EClientSocket created successfully.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error creating EClientSocket: {ex.Message}");
+    Console.WriteLine(ex.StackTrace);
+}
